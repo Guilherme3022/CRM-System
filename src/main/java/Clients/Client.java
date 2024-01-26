6f26a79f260f7fe04d23de51175a0263e70a127b
Guilherme 3022 <gui.bap22@gmail.com>
2024-01-26 17:05:04 -0300

    Updating client class 1

--- src/main/java/Clients/Client.java
+++ src/main/java/Clients/Client.java
@@ -1,16 +1,44 @@
 package Clients;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.*;
+import java.time.*;
 
 public class Client {
     private String ssn;
-    private String name;
+    private String fullName;
     private String email;
-    private char gender;
+    private String birthDate;
+    private String address;
+    private String phoneNumber;
+    private int age;
 
-    public Client(String ssn, String name, String email, char gender) {
+    public Client(String fullName, String birthDate, String ssn, String email, String phoneNumber, String address) {
         this.ssn = ssn;
-        this.name = name;
+        this.fullName = fullName;
         this.email = email;
-        this.gender = gender;
+        this.birthDate = birthDate;
+        try {
+            this.age =ageCalculator(birthDate);
+        }catch (IllegalArgumentException e){
+            System.out.println("Error: " + e.getMessage());
+        }
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+
+    }
+    private int ageCalculator(String birthDate){
+        try {
+            DateTimeFormatter americanFormat = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+            LocalDate dateOfBirth = LocalDate.parse(birthDate, americanFormat);
+            if (dateOfBirth.isAfter(LocalDate.now())) {
+                throw new RuntimeException("birth date in the future is not valid");
+            }
+            Period period = Period.between(dateOfBirth, LocalDate.now());
+            return period.getYears();
+        }catch (DateTimeParseException e ){
+            throw new RuntimeException("Invalid date format. Please use the format MM/dd/yyyy.");
+        }
     }
     public String getSsn(){
         return this.ssn;
@@ -19,11 +47,11 @@ public class Client {
         this.ssn = ssn;
     }
 
-    public String getName(){
-        return this.name;
+    public String getFullName(){
+        return this.fullName;
     }
-    public void setName(String name){
-        this.name =name;
+    public void setFullName(String fullName){
+        this.fullName =fullName;
     }
     public String getEmail(){
         return this.email;
@@ -31,10 +59,36 @@ public class Client {
     public void setEmail(String email){
         this.email =email;
     }
-    public char getGender(){
-        return this.gender;
+    public String getBirthDate() {
+        return this.birthDate;
     }
-    public void setGender(char gender){
-        this.gender =gender;
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+    public String getAddress() {
+        return this.address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public String getPhoneNumber() {
+        return this.phoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString(){
+        return "Client's data:\nName: "+getFullName()+"\nBirth date: "+getBirthDate()+"\nAge: "+getAge()+"\nSSN: "
+                +getSsn()+"\nEmail address: "+getEmail()+"\nPhone number: "+getPhoneNumber()+"\nZip address: "+getAddress();
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,11 @@
+import Clients.Client;
+
 public class Main {
     public static void main(String[] args) {
+        Client clientG = new Client("Guilherme Baptista da Silva","11/12/2001","00978074947","gui.bap22@gmail.com", "47997225195","rua joao");
+        System.out.println(clientG);
+        Client clientGu = new Client("Gustavo Baptista da Silva","10/26/2010","009781234947","gu.bap22@gmail.com", "479912325195","rua joao");
+        System.out.println("\n"+clientGu);
 
-        System.out.println("Hello world!");
     }
 }
